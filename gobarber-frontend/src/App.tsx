efe43c691484ed511deff73d3ac0eb9bcee92efd
IanMarcony <imarconyls@gmail.com>
2021-02-24 03:05:57 -0400

    Initial Project  Web

---
+++ gobarber-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Eae Galera!</h1>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
