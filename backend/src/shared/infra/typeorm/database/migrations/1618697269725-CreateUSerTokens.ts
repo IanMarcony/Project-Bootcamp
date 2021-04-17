7179fb99c636e855a309300dcf3dad17a8d4204b
IanMarcony <ianmarconysalazar@outlook.com>
2021-04-17 18:19:16 -0400

    Added more business rules with Password

--- backend/src/shared/infra/typeorm/database/migrations/1618697269725-CreateUSerTokens.ts
+++ backend/src/shared/infra/typeorm/database/migrations/1618697269725-CreateUSerTokens.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppoitments1612067574692
+export default class CreateUSerTokens1618697269725
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'user_tokens',
         columns: [
           {
             name: 'id',
@@ -15,15 +15,13 @@ export default class CreateAppoitments1612067574692
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
-            type: 'varchar',
-            isNullable: false,
+            name: 'token',
+            type: 'uuid',
+
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
-          {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
-          },
+          { name: 'user_id', type: 'uuid' },
           {
             name: 'created_at',
             type: 'timestamp',
@@ -35,11 +33,21 @@ export default class CreateAppoitments1612067574692
             default: 'now()',
           },
         ],
+        foreignKeys: [
+          {
+            name: 'TokenUser',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('user_tokens');
   }
 }
