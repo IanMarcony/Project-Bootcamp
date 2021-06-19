7bfddd3cd1b2455dd9e5a3b51a88f532fee7dc9b
IanMarcony <ianmarconysalazar@outlook.com>
2021-06-19 10:25:48 -0400

    feat: add email provider

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,10 +3,13 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from './MailProvider/models/IMailProvider';
-// import MailProvider from './MailProvider/implementations/MailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
-// container.registerSingleton<IMailProvider>('MailProvider', MailProvider);
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealMailProvider,
+);
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
